814da4f3e477e06232556417b7334b30982e0bb4
Ramil Domingo <ramil.domingo@connectedbusiness.com>
2018-05-08 15:35:00 +0800

    Added assembly information file for release build versioning

---
+++ ExecutableAssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyProduct("Connected Business")]
+[assembly: AssemblyCompany("Connected Business")]
+[assembly: AssemblyCopyright("Copyright © 2018 Connected Business, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisibleAttribute(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("18.2.*")]
---
+++ GlobalAssemblyInfo.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Security
+Imports System.Security.Permissions
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCompanyAttribute("Connected Business")> 
+<Assembly: AssemblyCopyrightAttribute("Copyright © 2018 Connected Business, All rights reserved.")> 
+<Assembly: AssemblyProductAttribute("Connected Business")> 
+<Assembly: ComVisible(True)> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("18.2.*")> 
